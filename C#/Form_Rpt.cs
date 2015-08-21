81839b76608fc3685b218aef78f9217e5e1ffebd
espressojp <espresso.goo.jp@gmail.com>
2015-08-21 17:31:24 +0900

    Add TN223949

---
+++ C#/Form_Rpt.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DSSCOMMasterLib;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form_Rpt : Form
+    {
+
+        public Form_Rpt()
+        {
+            InitializeComponent();
+        }
+
+        //レポートのSQLを表示
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string reportGuid = "796249EE4429B79CD62FC195F59A6764";
+
+            try
+            {
+
+                MSTRConn con1 = new MSTRConn();
+
+                IDSSSession oSession = con1.getSession();
+
+                IDSSSource oSource = oSession.ObjectSource;
+
+                IDSSReportSource oReportSource = oSession.ReportSource;
+
+                IDSSReportDefinition oReportDef = (IDSSReportDefinition)oSource.FindObject(reportGuid, EnumDSSObjectType.DssTypeReportDefinition,
+                                                           EnumDSSSourceFlags.DssSourceDefault, null, 0, 0);
+
+
+                IDSSReportInstance oReportInstance = oReportSource.ExecuteDefinition(oReportDef, EnumDSSReportActions.DssActionExecuteSQL, EnumDSSReportFlags.DssReportRunSynch, null, 0, 0);
+
+                //IDSSReportInstance oReportInstance = oReportSource.NewInstance(oReportDef);
+                //oReportSource.Execute(oReportInstance, EnumDSSReportActions.DssActionGenerateSQL, EnumDSSReportFlags.DssReportLocalOnly,null,0,0);
+                //oReportSource.GenerateSQL(oReportInstance,EnumDSSReportFlags.DssReportCacheDefault,null,0,0);
+
+                int i =0;
+                while ((int)(oReportInstance.State & EnumDSSReportStates.DssActionGenerateSQLDone) != 2)
+                {
+                    Thread.Sleep(100);
+                    i++;
+                    if (i == 10) break;
+                }
+
+                Console.WriteLine(i);
+
+                if (i != 10) { 
+                    foreach (IDSSSQL sql in oReportInstance.SQLs)
+                    {
+                        Console.WriteLine(sql.DisplaySQL);
+                    }
+                }
+
+                con1.close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message.ToString());
+            }
+        }
+
+        //Extended Data Access (XDA) reporting (Freeform SQL or Query Builder).
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string reportGuid = "1ACE8C37484AE0EBD81271BB88B63013";
+
+            try
+            {
+                MSTRConn con1 = new MSTRConn();
+
+                IDSSSession oSession = con1.getSession();
+
+                IDSSSource oSource = oSession.ObjectSource;
+
+                IDSSReportSource oReportSource = oSession.ReportSource;
+
+                IDSSReportDefinition4 oReportDef = (IDSSReportDefinition4)oSource.FindObject(reportGuid, EnumDSSObjectType.DssTypeReportDefinition,
+                                                           EnumDSSSourceFlags.DssSourceDefault, null, 0, 0);
+
+
+                Console.WriteLine(oReportDef.XDAType);
+
+                IDSSTable3 oTable = (IDSSTable3)oReportDef.SourceTable;
+
+                Console.WriteLine(oTable.XDADefinition);
+
+                con1.close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message.ToString());
+            }
+
+        }
+
+
+
+    }
+}
---
+++ C#/SessionClass1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DSSCOMMasterLib;
+
+namespace WindowsFormsApplication1
+{
+
+    public class MSTRConn
+    {
+        private const string MSTRServer = "JPN-AHORIUCHI2";
+        private const string MSTRLogin = "Administrator";
+        private const string MSTRPwd = "";
+
+        private DSSDataSource oServerDS;
+        private IDSSSession oSession;
+
+
+        public MSTRConn()
+        {
+
+            oServerDS = new DSSDataSource();
+            oServerDS.Type = EnumDSSDataSourceType.DssDataSourceTypeServer;
+            oServerDS.Mode = EnumDSSConnectionMode.DssConnectionModeServerAccess;
+            oServerDS.AuthMode = EnumDSSAuthModes.DssAuthStandard;
+            oServerDS.Location = MSTRServer;
+            oServerDS.port = 34952;
+            oServerDS.login = MSTRLogin;
+            oServerDS.Passwd = MSTRPwd;
+
+            oServerDS.Init();
+
+        }
+
+
+        public IDSSSession getSession()
+        {
+            IDSSDataSource oProject = oServerDS.Enumerator.Item(1);　 //ItemByProjectName
+
+            oProject.Init();
+            //oSession = oServerDS.CreateSession();
+
+            oSession = oProject.CreateSession();
+
+            return oSession;
+
+        }
+
+
+        public void close()
+        {
+
+            if (oSession != null)
+            {
+                Console.WriteLine("closing..");
+                oServerDS.Reset();
+                oServerDS = null;
+                oSession.Close();
+                oSession = null;
+            }
+
+
+        }
+    }
+}
